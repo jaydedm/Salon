89fd22d707a939d3f0c4dfeab4ba56e4e2716ea0
Jayde Mitchell <jayde@jaydemitchell.com>
2023-11-21 14:43:43 -0800

    Progress

--- src/pages/Company/AboutUs/sections/Information.js
+++ src/pages/Company/AboutUs/sections/Information.js
@@ -21,15 +21,27 @@ import Grid from '@mui/material/Grid'
 import MKBox from 'components/MKBox'
 
 // Otis Kit PRO examples
-import DefaultInfoCard from 'examples/Cards/InfoCards/DefaultInfoCard'
+// import DefaultInfoCard from 'examples/Cards/InfoCards/DefaultInfoCard'
 import CenteredBlogCard from 'examples/Cards/BlogCards/CenteredBlogCard'
+import FaqPage from 'layouts/pages/support/faq'
 
 function SalonInformation () {
   return (
     <MKBox component="section" py={12}>
       <Container>
         <Grid container spacing={3} alignItems="center">
-          <Grid item xs={12} lg={6}>
+            <CenteredBlogCard
+              image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+              title="Welcome to Penelope & Watson"
+              description="A haven for salon, barber, and esthetician services. Our space has undergone a refreshing transformation, and we are thrilled to bring you a range of exceptional services to cater to your unique needs."
+              action={{
+                type: 'external',
+                route: 'https://www.vagaro.com//Users/BusinessWidget.aspx?enc=MMLjhIwJMcwFQhXLL7ifVLI82XKP4EgxOtbbrGbm4jZjcWdYw9IzYcvzEFqLd4lj0+FJ687rN1J1l6DFrUqPjprYVemBRnMBGTkiMHY5xHAdeisLuoe9bZgs+39Q1Q8aFzujirrQeEHm1ommxMF5/BAGrSS+Qq88odv8EY03uHlZx+faOFh2vxiSbea8VjxX15+7SlUMDz4oHDdrvif/h8awqX8cIKg0AdOjtf8p8h4/iJ7D6hN7nV84OcXARVPSQXitjAO2UshlnIjo8hSLM1iDGqkMBnug+fi7ieQtsYTz4s3CGzVwyKk+4voguWJEVq/dBh13Tph6mc6/5hgtoiUeJ736frDCxVRuEQ0EO4dOJ7K9tsqkIr53UIqcSLc4',
+                color: 'gold',
+                label: 'Book Now'
+              }}
+            />
+          {/* <Grid item xs={12} lg={6}>
             <Grid container justifyContent="flex-start">
               <Grid item xs={12} md={6}>
                 <MKBox mb={5}>
@@ -48,65 +60,53 @@ function SalonInformation () {
                         <p>Thank you for choosing Penelope & Watson as your destination. We look forward to pampering you and bringing out the best in you. Let&apos;s embark on a journey of beauty and self-care together!</p>
                       </div>}
                   />
-              </MKBox>
+                </MKBox>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <MKBox mb={5}>
+                  <DefaultInfoCard
+                    icon="query_builder"
+                    title="Operating Hours"
+                    color='pink'
+                    description={
+                      <div>
+                        <p>
+                          At our establishment, we offer personalized service experiences tailored to your needs. To ensure we dedicate our full attention to you, we operate by appointment only. To secure your spot and embark on your journey of relaxation and beauty, simply head over to our services or team page, where you&apos;ll find a convenient link to book your appointment.
+                        </p>
+                        <br></br>
+                        <p>While we cherish the opportunity to serve walk-in clients, please be aware that availability and pricing for walk-in appointments may vary. For a seamless and tailored experience, we recommend booking in advance, allowing us to curate the perfect treatment or service just for you.</p>
+                        <br></br>
+                        <p>Experience the luxury of personalized care and attention at our inviting space. Unlock a world of rejuvenation and indulgence by reserving your appointment today. We eagerly await the pleasure of serving you and exceeding your expectations with our exceptional services.</p>
+                      </div>
+                    }
+                  />
+                </MKBox>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <MKBox mb={{ xs: 5, md: 0 }}>
+                  <DefaultInfoCard
+                    icon="announcement"
+                    color='pink'
+                    title="Booking Policy"
+                    description="We get insulted by others, lose trust for those We get back freezes"
+                  />
+                </MKBox>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <MKBox mb={{ xs: 5, md: 0 }}>
+                  <DefaultInfoCard
+                    icon="3p"
+                    color='pink'
+                    title="Improved platform"
+                    description="We get insulted by others, lose trust for those We get back freezes"
+                  />
+                </MKBox>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <MKBox mb={5}>
-                <DefaultInfoCard
-                  icon="query_builder"
-                  title="Operating Hours"
-                  color='pink'
-                  description={
-                    <div>
-                      <p>
-                      At our establishment, we offer personalized service experiences tailored to your needs. To ensure we dedicate our full attention to you, we operate by appointment only. To secure your spot and embark on your journey of relaxation and beauty, simply head over to our services or team page, where you&apos;ll find a convenient link to book your appointment.
-                      </p>
-                      <br></br>
-                      <p>While we cherish the opportunity to serve walk-in clients, please be aware that availability and pricing for walk-in appointments may vary. For a seamless and tailored experience, we recommend booking in advance, allowing us to curate the perfect treatment or service just for you.</p>
-                      <br></br>
-                      <p>Experience the luxury of personalized care and attention at our inviting space. Unlock a world of rejuvenation and indulgence by reserving your appointment today. We eagerly await the pleasure of serving you and exceeding your expectations with our exceptional services.</p>
-                    </div>
-                  }
-                />
-              </MKBox>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <MKBox mb={{ xs: 5, md: 0 }}>
-                <DefaultInfoCard
-                  icon="announcement"
-                  color='pink'
-                  title="Booking Policy"
-                  description="We get insulted by others, lose trust for those We get back freezes"
-                />
-              </MKBox>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <MKBox mb={{ xs: 5, md: 0 }}>
-                <DefaultInfoCard
-                  icon="3p"
-                  color='pink'
-                  title="Improved platform"
-                  description="We get insulted by others, lose trust for those We get back freezes"
-                />
-              </MKBox>
-            </Grid>
-          </Grid>
+          </Grid> */}
         </Grid>
-        <Grid item xs={12} lg={4} sx={{ ml: 'auto', mt: { xs: 3, lg: 0 } }}>
-          <CenteredBlogCard
-            image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-            title="Welcome to Penelope & Watson"
-            description="A haven for salon, barber, and esthetician services. Our space has undergone a refreshing transformation, and we are thrilled to bring you a range of exceptional services to cater to your unique needs."
-            action={{
-              type: 'external',
-              route: 'https://www.vagaro.com//Users/BusinessWidget.aspx?enc=MMLjhIwJMcwFQhXLL7ifVLI82XKP4EgxOtbbrGbm4jZjcWdYw9IzYcvzEFqLd4lj0+FJ687rN1J1l6DFrUqPjprYVemBRnMBGTkiMHY5xHAdeisLuoe9bZgs+39Q1Q8aFzujirrQeEHm1ommxMF5/BAGrSS+Qq88odv8EY03uHlZx+faOFh2vxiSbea8VjxX15+7SlUMDz4oHDdrvif/h8awqX8cIKg0AdOjtf8p8h4/iJ7D6hN7nV84OcXARVPSQXitjAO2UshlnIjo8hSLM1iDGqkMBnug+fi7ieQtsYTz4s3CGzVwyKk+4voguWJEVq/dBh13Tph6mc6/5hgtoiUeJ736frDCxVRuEQ0EO4dOJ7K9tsqkIr53UIqcSLc4',
-              color: 'gold',
-              label: 'Book Now'
-            }}
-          />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <FaqPage />
     </MKBox >
   )
 }
--- src/pages/Support/Faq/index.js
+++ src/pages/Support/Faq/index.js
@@ -25,31 +25,16 @@ import MKBox from 'components/MKBox'
 import MKTypography from 'components/MKTypography'
 
 // Otis Kit PRO examples
-import DefaultNavbar from 'examples/Navbars/DefaultNavbar'
-import SimpleFooter from 'examples/Footers/SimpleFooter'
 import SimpleInfoCard from 'examples/Cards/InfoCards/SimpleInfoCard'
 
 // HelpCenter page components
 import FaqCollapse from 'pages/Support/Faq/components/FaqCollapse'
-
-// Routes
-import routes from 'routes'
 
 function Faq () {
   const [collapse, setCollapse] = useState(false)
 
   return (
     <>
-      <DefaultNavbar
-        routes={routes}
-        action={{
-          type: 'external',
-          route: 'https://material-ui.com/store/items/otis-kit-pro-material-kit-react/',
-          label: 'buy now',
-          color: 'info'
-        }}
-        sticky
-      />
       <MKBox component="section" pt={20} pb={12}>
         <Container sx={{ mb: 8 }}>
           <Grid container spacing={3}>
@@ -93,87 +78,31 @@ function Faq () {
                   mx={2}
                 >
                   <MKTypography variant="h3" color="white">
-                    FAQ
-                  </MKTypography>
-                  <MKTypography variant="body2" color="white" opacity={0.8}>
-                    Last modified: Sept 07 2021
+                    Important Information
                   </MKTypography>
                 </MKBox>
                 <MKBox p={6}>
                   <MKTypography variant="h5" my={3}>
-                    Basics
+                    Policies
                   </MKTypography>
                   <FaqCollapse
-                    title="How do I order?"
+                    title="Booking Policy"
                     open={collapse === 1}
                     onClick={() => (collapse === 1 ? setCollapse(false) : setCollapse(1))}
                   >
-                    We&apos;re not always in the position that we want to be at. We&apos;re
-                    constantly growing. We&apos;re constantly making mistakes. We&apos;re constantly
-                    trying to express ourselves and actualize our dreams. If you have the
-                    opportunity to play this game of life you need to appreciate every moment. A lot
-                    of people don&apos;t appreciate the moment until it&apos;s passed.
+                    To secure your preferred service, we require a non-refundable deposit and a valid card on file at the time of booking.
+                    The deposit will be credited toward your total payment during the final transaction.
+                    This policy ensures efficient accommodation for all clients and upholds our commitment to exceptional experiences.
+                    Thank you for your understanding and cooperation. We look forward to welcoming you and delivering a memorable and personalized service that exceeds your expectations.
                   </FaqCollapse>
                   <FaqCollapse
-                    title="How can i make the payment?"
+                    title="Guest Policy"
                     open={collapse === 2}
                     onClick={() => (collapse === 2 ? setCollapse(false) : setCollapse(2))}
                   >
-                    It really matters and then like it really doesn&apos;t matter. What matters is
-                    the people who are sparked by it. And the people who are like offended by it, it
-                    doesn&apos;t matter. Because it&apos;s about motivating the doers. Because
-                    I&apos;m here to follow my dreams and inspire other people to follow their
-                    dreams, too. We&apos;re not always in the position that we want to be at.
-                    We&apos;re constantly growing. We&apos;re constantly making mistakes. We&apos;re
-                    constantly trying to express ourselves and actualize our dreams. If you have the
-                    opportunity to play this game of life you need to appreciate every moment. A lot
-                    of people don&apos;t appreciate the moment until it&apos;s passed.
-                  </FaqCollapse>
-                  <FaqCollapse
-                    title="How much time does it take to receive the order?"
-                    open={collapse === 3}
-                    onClick={() => (collapse === 3 ? setCollapse(false) : setCollapse(3))}
-                  >
-                    The time is now for it to be okay to be great. People in this world shun people
-                    for being great. For being a bright color. For standing out. But the time is now
-                    to be okay to be the greatest you. Would you believe in what you believe in, if
-                    you were the only one who believed it? If everything I did failed - which it
-                    doesn&apos;t, it actually succeeds - just the fact that I&apos;m willing to fail
-                    is an inspiration. People are so scared to lose that they don&apos;t even try.
-                    Like, one thing people can&apos;t say is that I&apos;m not trying, and I&apos;m
-                    not trying my hardest, and I&apos;m not trying to do the best way I know how.
-                  </FaqCollapse>
-                  <FaqCollapse
-                    title="Can I resell the products?"
-                    open={collapse === 4}
-                    onClick={() => (collapse === 4 ? setCollapse(false) : setCollapse(4))}
-                  >
-                    I always felt like I could do anything. That&apos;s the main thing people are
-                    controlled by! Thoughts- their perception of themselves! They&apos;re slowed
-                    down by their perception of themselves. If you&apos;re taught you can&apos;t do
-                    anything, you won&apos;t do anything. I was taught I could do everything.
-                    <br />
-                    <br />
-                    If everything I did failed - which it doesn&apos;t, it actually succeeds - just
-                    the fact that I&apos;m willing to fail is an inspiration. People are so scared
-                    to lose that they don&apos;t even try. Like, one thing people can&apos;t say is
-                    that I&apos;m not trying, and I&apos;m not trying my hardest, and I&apos;m not
-                    trying to do the best way I know how.
-                  </FaqCollapse>
-                  <FaqCollapse
-                    title="Where do I find the shipping details?"
-                    open={collapse === 5}
-                    onClick={() => (collapse === 5 ? setCollapse(false) : setCollapse(5))}
-                  >
-                    There&apos;s nothing I really wanted to do in life that I wasn&apos;t able to
-                    get good at. That&apos;s my skill. I&apos;m not really specifically talented at
-                    anything except for the ability to learn. That&apos;s what I do. That&apos;s
-                    what I&apos;m here for. Don&apos;t be afraid to be wrong because you can&apos;t
-                    learn anything from a compliment. I always felt like I could do anything.
-                    That&apos;s the main thing people are controlled by! Thoughts- their perception
-                    of themselves! They&apos;re slowed down by their perception of themselves. If
-                    you&apos;re taught you can&apos;t do anything, you won&apos;t do anything. I was
-                    taught I could do everything.
+                    Please have accompanying guests wait quietly in our lobby during your appointment.
+                    To maintain a tranquil atmosphere, we request that no young children accompany you.
+                    Thank you for understanding and working together to preserve the serenity of our space, ensuring a peaceful environment for all guests.
                   </FaqCollapse>
                   <MKTypography variant="h5" mt={6} mb={3}>
                     Account & Settings
@@ -271,9 +200,6 @@ function Faq () {
             </Grid>
           </Grid>
         </Container>
-      </MKBox>
-      <MKBox pt={{ xs: 0, lg: 3 }} pb={3}>
-        <SimpleFooter />
       </MKBox>
     </>
   )
